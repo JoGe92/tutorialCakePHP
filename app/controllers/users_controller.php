07b680e3bd6408e1930063118eeb1edfebc60e21
Jorge Puente <jorge.alejandro.puente@gmail.com>
2015-03-11 13:01:59 -0600

    First Commit

---
+++ app/controllers/users_controller.php
@@ -0,0 +1,21 @@
+<?php 
+
+	class UsersController extends AppController {
+    
+	var $layout = 'login';
+    var $uses = array();
+
+    function index(){
+
+    }
+
+    function login(){
+
+    }
+
+    function logout(){
+
+    }
+}
+
+?>
